b984daa48d89233523ba2e898172a4c9d36a899e
kyala16 <aretem.myakishev.2018@mail.ru>
2022-03-14 22:21:47 +0300

    file with comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,10 +2,10 @@
 #include <string>
 
 int main(){
-	std::string name;
-	std::cin >> name;
-	std::cout << "Hello world from " << name << std::endl;
-	return 0;
+	std::string name;  //Create new object
+	std::cin >> name;  //Read from console name what we want
+	std::cout << "Hello world from " << name << std::endl;  //Main task
+	return 0;  //End of main
 }
 
 
