5c2af97269d82c280ee489660fc92cb0bc541ecc
kyala16 <aretem.myakishev.2018@mail.ru>
2022-03-15 00:23:20 +0300

    File with new right format(Mozilla)

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <string>
 
-int main(){
-	std::string name;  //Create new object
-	std::cin >> name;  //Read from console name what we want
-	std::cout << "Hello world from " << name << std::endl;  //Main task
-	return 0;  //End of main
+int
+main()
+{
+  std::string name; // Create new object
+  std::cin >> name; // Read from console name what we want
+  std::cout << "Hello world from " << name << std::endl; // Main task
+  return 0;                                              // End of main
 }
-
-
